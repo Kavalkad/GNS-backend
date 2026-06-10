e4a22a582c01152a5c7ec62ce94f12790027b819
Kavalkad <github.andy2006@gmail.com>
2026-06-10 22:04:11 +0300

    refactor: refactoring db configuration

---
+++ Migrations/20260604073538_RefactoringUselessProperties.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefactoringUselessProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_GamingPlaces_GamingPlaceEntityId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_GamingPlaceEntityId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "GamingPlaceEntityId",
+                table: "Games");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "GamingPlaceEntityId",
+                table: "Games",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GamingPlaceEntityId",
+                table: "Games",
+                column: "GamingPlaceEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_GamingPlaces_GamingPlaceEntityId",
+                table: "Games",
+                column: "GamingPlaceEntityId",
+                principalTable: "GamingPlaces",
+                principalColumn: "Id");
+        }
+    }
+}
